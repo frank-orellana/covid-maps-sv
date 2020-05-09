b2ba0e37cf5b8bca4f651628dd9f8dd61ac7b5ab
Frank Orellana <lando.orellana@gmail.com>
2020-05-09 18:55:06 -0500

    mejorada configuración dev-prod

--- src/tools/config.ts
+++ src/tools/config.ts
@@ -1,4 +1,6 @@
+console.log('config:',process.env.NODE_ENV);
+
 export default {
-	api_url : 'http://localhost:3000',
-	app_name : 'tritium maps',
+	api_url : process.env.NODE_ENV === 'production'? 'https://tritium.cl:3000': 'http://localhost:3000',
+	app_name : 'tritium maps'
 }
