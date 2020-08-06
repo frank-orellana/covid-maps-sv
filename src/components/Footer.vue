a4de4142459295917703a456f35443595a6e4993
Frank Orellana <lando.orellana@gmail.com>
2020-08-05 21:25:43 -0500

    Footer repositioning

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+	<footer class="copy">
+		<p class="float-right"><a href="#">Regresar arriba</a></p>
+		<p>&copy; 2017-2020 TRITIUM, S.p.A. &middot; <a href="https://tritium.cl/privacy.html#spanish">Política de privacidad</a> &middot; <a
+				href="https://tritium.cl/disclaimer.html#spanish">Términos y condiciones</a></p>
+		<p>Desarrollado por TRITIUM, S.p.A.</p>
+	</footer>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="home">
     <Mapa/>
-    <footer class="copy">
-			<p class="float-right"><a href="#">Regresar arriba</a></p>
-			<p>&copy; 2017-2020 TRITIUM, S.p.A. &middot; <a href="https://tritium.cl/privacy.html#spanish">Política de privacidad</a> &middot; <a
-				 href="https://tritium.cl/disclaimer.html#spanish">Términos y condiciones</a></p>
-			<p>Desarrollado por TRITIUM, S.p.A.</p>
-		</footer>
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Mapa from '@/components/Mapa.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    Mapa
+    Mapa, Footer
   }
 }
 </script>
