20c282056f5fd5fb0bfa989722b980e1de5d9234
Frank Orellana <lando.orellana@gmail.com>
2022-03-17 22:58:01 -0400

    updated service url

--- src/tools/config.ts
+++ src/tools/config.ts
@@ -1,6 +1,6 @@
 console.log('config:',process.env.NODE_ENV);
 
 export default {
-	api_url : process.env.NODE_ENV === 'production'? 'https://tritium.cl:3000': `http://${location.hostname}:3000`,
+	api_url : process.env.NODE_ENV === 'production'? 'https://intranet.tritium.cl:3000': `http://${location.hostname}:3000`,
 	app_name : 'tritium maps'
 }
